0545b092222237c2a97120845826a901404e09b4
Severino Sperini <Severino.Sperini@wuerth-phoenix.com>
2023-08-30 22:44:39 +0200

    semplici aggiustamenti su MergeSort

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -2,42 +2,41 @@ import java.util.Arrays;
 
 public class MergeSort {
     public static void mergeSort(int[] array) {
-        int length = array.length;
-        if (length <= 1) return; // base case
-        int middle = length / 2;
+        if (array.length <= 1) return; // base case
+        int middle = array.length / 2;
+
         int[] leftArray = Arrays.copyOfRange(array, 0, middle);
-        int[] rightArray = Arrays.copyOfRange(array, middle, length);
+        int[] rightArray = Arrays.copyOfRange(array, middle, array.length);
 
         mergeSort(leftArray);
         mergeSort(rightArray);
-        //System.out.println(Arrays.toString(leftArray) + " " + Arrays.toString(rightArray));
+
         merge(leftArray, rightArray, array);
     }
 
     public static void merge(int[] leftArray, int[] rightArray, int[] array) {
-        int leftSize = array.length / 2;
-        int rightSize = array.length - leftSize;
         int i = 0, l = 0, r = 0; //indices
 
         //check the conditions for merging
-        while(l < leftSize && r < rightSize) {
+        while(l < leftArray.length && r < rightArray.length) {
             if(leftArray[l] < rightArray[r]) {
                 array[i] = leftArray[l];
                 i++;
                 l++;
-            }
-            else {
+            } else {
                 array[i] = rightArray[r];
                 i++;
                 r++;
             }
         }
-        while(l < leftSize) {
+
+        while(l < leftArray.length) {
             array[i] = leftArray[l];
             i++;
             l++;
         }
-        while(r < rightSize) {
+
+        while(r < rightArray.length) {
             array[i] = rightArray[r];
             i++;
             r++;
@@ -45,11 +44,10 @@ public class MergeSort {
     }
 
     public static void main(String[] args) {
-        int[] array = {12, 11, 13, 5, 6, 7};
+        int[] array = {12, 11, 13, 5, 6, 7, 1, 8, 0, 3};
         mergeSort(array);
         for (int j : array) {
             System.out.print(j + " ");
         }
-
     }
 }
